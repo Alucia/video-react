9ddbac39c1b0ae74ca2195ad4e56a0c53c9ee5b8
Alucia <analuciacarhuaz@gmail.com>
2018-05-19 12:40:23 -0500

    base file and media component

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Media from './src/playlist/components/media';
+
+const container = document.getElementById('app')
+
+//ReactDOM.render(que voy a renderizar, donde lo haré);
+// const holaMundo = <h1>Hola Estudiante!</h1>
+render(<Media />, app);
