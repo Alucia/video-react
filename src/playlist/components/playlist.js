8e0dd7cc349a99c4f29ac25ff01f85c7cc153a11
Alucia <analuciacarhuaz@gmail.com>
2018-05-19 20:25:16 -0500

    create icons with components

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import Media from './media.js';
 import './playlist.css'
+import Play from '../../icons/components/play';
+import FullScreen from '../../icons/components/full-screen'
+import Pause from '../../icons/components/pause'
+import Volume from '../../icons/components/volume'
 
 function Playlist(props) {
   const playlist = props.data.categories[0].playlist
   console.log(props.data)
   return (
     <div className="Playlist">
+      <Play 
+        size={50}
+        color="red"
+      />
+      <FullScreen 
+        size={40}
+        color="black"
+      />
+      <Pause 
+        size={40}
+        color="black"
+      />
+      <Volume 
+        size={50}
+        color="green"
+      />
+      <br />
       {
         playlist.map((item) => {
           return <Media {...item} key={item.id} />
