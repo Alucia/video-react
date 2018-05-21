35fb575c78c1fa3296d68cd619f474143c46520a
Alucia <analuciacarhuaz@gmail.com>
2018-05-21 18:31:02 -0500

    events, change value

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -8,11 +8,19 @@ import './search.css';
 // }
 
 const Search = (props) => (
-  <form className="Search">
+  <form 
+    className="Search"
+    onSubmit={props.handleSubmit}
+  >
     <input 
+      ref={props.setRef}
       type="text" 
       placeholder="Busca tu video favorito"
       className="Search-input"
+      name="search"
+      value={props.value}
+      onChange={props.handleChange}
+      // defaultValue="Luis Fonsi"
     />
   </form>
 )
